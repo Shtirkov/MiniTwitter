d8890e8f87404ffcfeaeb2323eb9a4ed1d9b6177
Shtirkov <shtirkov96@gmail.com>
2025-08-16 16:22:21 +0300

    initial commit

---
+++ MiniTwitter/Models/TwitterContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniTwitter.Models
+{
+    public class TwitterContext : DbContext
+    {
+        public TwitterContext(DbContextOptions<TwitterContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
