14625d2d41d41fd60386ec78fc440c6d13278bf4
Shtirkov <shtirkov96@gmail.com>
2025-09-16 20:31:17 +0300

    Reduce points of contact with the database in comments service

--- MiniTwitter/Services/CommentsService.cs
+++ MiniTwitter/Services/CommentsService.cs
@@ -1,43 +1,50 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MiniTwitter;
 using MiniTwitter.Interfaces;
 using MiniTwitter.Models;
 using MiniTwitter.RequestModels;
 
-namespace MiniTwitter.Services
+public class CommentsService : ICommentsService
 {
-    public class CommentsService : ICommentsService
+    private readonly TwitterContext _context;
+
+    public CommentsService(TwitterContext context)
     {
-        private readonly TwitterContext _context;
+        _context = context;
+    }
+    private IQueryable<Comment> CommentsQuery()
+    {
+        return _context.Comments.AsNoTracking().Include(c => c.Author);
+    }
 
-        public CommentsService(TwitterContext context)
-        {
-            _context = context;
-        }
+    private Task<Comment?> GetCommentByIdAsync(int id)
+    {
+       return CommentsQuery().FirstOrDefaultAsync(c => c.Id == id);
+    }
 
-        public async Task AddAsync(Comment comment)
-        {
-            await _context.Comments.AddAsync(comment);
-        }
+    public async Task AddAsync(Comment comment)
+    {
+        await _context.Comments.AddAsync(comment);
+    }
 
-        public Comment Edit(Comment comment, EditCommentRequestDto commentDto)
-        {
-            comment.Content = commentDto.Content;
-            return comment;
-        }
+    public Comment Edit(Comment comment, EditCommentRequestDto commentDto)
+    {
+        comment.Content = commentDto.Content;
+        return comment;
+    }
 
-        public async Task<Comment?> GetAsync(int id)
-        {
-            return await _context.Comments.Include(c => c.Author).FirstOrDefaultAsync(c => c.Id == id);
-        }
+    public async Task<Comment?> GetAsync(int id)
+    {
+        return await GetCommentByIdAsync(id);
+    }
 
-        public void Remove(Comment comment)
-        {
-            _context.Remove(comment);
-        }
+    public void Remove(Comment comment)
+    {
+        _context.Remove(comment);
+    }
 
-        public async Task SaveChangesAsync()
-        {
-            await _context.SaveChangesAsync();
-        }
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
     }
 }
